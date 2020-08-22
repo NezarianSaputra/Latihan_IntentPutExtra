a1737227b2ee8546d2979c73c891540358cb223d
NezarianSaputra <nezarian16.siswa@smkn4bdg.sch.id>
2020-08-22 08:15:55 +0700

    File Java Latihan Intent dengan Put Extra

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.latihan_intent1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText name =(EditText) findViewById(R.id.edit_textData);
+        Button button = (Button) findViewById(R.id.button_submitIntent);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+
+    }
+}
